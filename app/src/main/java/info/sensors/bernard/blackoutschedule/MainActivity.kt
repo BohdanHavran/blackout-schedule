bca6cdc91376473a3e0762066e8f357ec2d3c5bd
Yaroslav <hrubyaroslav@gmail.com>
2024-05-25 12:13:05 +0300

    feature: language change іуе setting are ready

--- app/src/main/java/info/sensors/bernard/blackoutschedule/MainActivity.kt
+++ app/src/main/java/info/sensors/bernard/blackoutschedule/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
+import android.content.res.Configuration
 import android.media.MediaPlayer
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -17,6 +18,7 @@ import android.widget.Toast
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     private var goToNewActivity = false
@@ -59,6 +61,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val sharedPreferencesForLanguage = getSharedPreferences("Settings", Context.MODE_PRIVATE)
+        val language = sharedPreferencesForLanguage.getString("My_Lang", "deff")
+        if (language != "deff"){
+            setLocale(this, language!!)
+        }
         setContentView(R.layout.activity_main)
         val click = MediaPlayer.create(this, R.raw.button_sound)
         val serviceIntent = Intent(this, MusicService::class.java)
@@ -67,6 +74,7 @@ class MainActivity : AppCompatActivity() {
         val sharedPreferencesForMusic = getSharedPreferences("SoundState", Context.MODE_PRIVATE)
         val clickSound = sharedPreferencesForMusic.getFloat("buttonSound", 1f)
         click.setVolume(clickSound, clickSound)
+        val sharedPreferencesForFade = getSharedPreferences("Fade", Context.MODE_PRIVATE)
 
         val sharedPreferencesBrightness = getSharedPreferences("Brightness", Context.MODE_PRIVATE)
         if (sharedPreferencesBrightness.getBoolean("isUsingManualBrightness", false)){
@@ -76,6 +84,7 @@ class MainActivity : AppCompatActivity() {
             layoutParams.screenBrightness = brightnessValue
             window.attributes = layoutParams
         }
+
         val buttonQuest: ImageButton = findViewById(R.id.questButton)
         val buttonSettings: ImageButton = findViewById(R.id.settingsButton)
         val buttonFirstGroup: ImageButton = findViewById(R.id.groupFirstButton)
@@ -107,8 +116,13 @@ class MainActivity : AppCompatActivity() {
             goToNewActivity = true
             val intent = Intent(this, SettingsActivity::class.java)
             startActivity(intent)
+            when(sharedPreferencesForFade.getInt("fadeStatus", 50)){
+                in 21..40 -> overridePendingTransition(R.anim.fade_in_realy_slow, R.anim.fade_out_realy_slow)
+                in 41..60 -> overridePendingTransition(R.anim.fade_in_slow, R.anim.fade_out_slow)
+                in 61..80 -> overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+                in 81..100 -> overridePendingTransition(R.anim.fade_in_fast, R.anim.fade_out_fast)
+            }
             finish()
-            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
         }
         buttonFirstGroup.setOnClickListener {
             click.start()
@@ -142,6 +156,19 @@ class MainActivity : AppCompatActivity() {
 
 
 
+    private fun setLocale(context: Context, language: String) {
+        val locale = Locale(language)
+        Locale.setDefault(locale)
+        val config = Configuration()
+        config.setLocale(locale)
+        context.resources.updateConfiguration(config, context.resources.displayMetrics)
+    }
+    private fun saveLocale(language: String) {
+        val sharedPreferences = getSharedPreferences("Settings", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("My_Lang", language)
+        editor.apply()
+    }
     override fun onPause() {
         super.onPause()
         hideUi()
